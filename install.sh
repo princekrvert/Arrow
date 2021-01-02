78ba1de6be007dd814a07b51c5263f0bfc018b9a
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2021-01-02 18:45:25 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#@Author prince kumar
+#Date 10 dec 2020
+#VersionV1.0
+apt update && apt upgrade
+apt-git install python
+apt-git install python2
+pkg install python3
+pip3 install requests
+pip3 install rendom
+pip3 install threading
+pip3 install time
+clear 
+python3 arrow.py
